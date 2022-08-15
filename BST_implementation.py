59a871fea3934d7fcd59522814f70bc650617157
chokrihamza <69900730+chokrihamza@users.noreply.github.com>
2022-08-15 19:50:19 +0200

    delete operation  in BST
    delete operation in BST

--- BST_implementation.py
+++ BST_implementation.py
@@ -37,6 +37,8 @@ class BST:
                 self.rchild.search(data)
             else:
                 print("Node is not present in te tree !!!")
+                
+                
      def preorder(self):
      
         # root -> left -> right
@@ -45,6 +47,10 @@ class BST:
             self.lchild.preorder()
         if self.rchild:
             self.rchild.preorder()
+            
+            
+            
+            
     def inorder(self):
         """ left -> root -> right"""
         if self.lchild:
@@ -52,6 +58,10 @@ class BST:
         print(self.key)
         if self.rchild:
             self.rchild.inorder()
+            
+            
+            
+            
     def postorder(self):
       
         if self.lchild :
@@ -60,6 +70,58 @@ class BST:
            self.rchild.postorder()
         print(self.key)
         
+        
+        
+    def delete(self,data):
+        if self.key is None:
+            print("Tree is empty !!!")
+        if data<self.key:
+            if self.lchild:
+                self.lchild=self.lchild.delete(data)
+            else:
+                print("the node in not presented in the tree")
+        elif data>self.key:
+            if self.rchild:
+                self.rchild=self.rchild.delete(data)
+            else:
+                print("the node in not presented in the tree")
+        else:
+            if self.lchild is None:
+                temp=self.rchild
+                self=None
+                return temp
+            if self.rchild is None:
+                temp=self.lchild
+                self=None
+                return temp
+            node=self.rchild
+            while node.lchild:
+                node=node.lchild
+                
+            self.key=node.key
+            self.rchild=self.rchild.delete(node.key)
+        return self
+                
+
+root=BST(10)
+#root.lchild=BST(5)
+#print(root.key,root.lchild.lchild,root.rchild)
+list1=[6,3,1,6,98,3,7]
+for i in list1:
+    root.insert(i)
+
+#root.search(2)
+print("preorder traversal")
+root.preorder()
+print("inorder traversal")
+root.inorder()
+print("postorder traversal")
+
+root.delete(6)
+root.delete(7)
+root.delete(98)
+root.postorder()
+    
 
 root=BST(10)
 #root.lchild=BST(5)
