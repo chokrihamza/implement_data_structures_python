de09f826a18e2c6aabbc41cde2576bf5722fe07f
chokrihamza <69900730+chokrihamza@users.noreply.github.com>
2022-08-15 19:02:20 +0200

    add inorder,postorder,preorder traversal
    inorder,postorder,preorder traversal

--- BST_implementation.py
+++ BST_implementation.py
@@ -37,7 +37,44 @@ class BST:
                 self.rchild.search(data)
             else:
                 print("Node is not present in te tree !!!")
-                
+     def preorder(self):
+     
+        # root -> left -> right
+        print(self.key)
+        if self.lchild:
+            self.lchild.preorder()
+        if self.rchild:
+            self.rchild.preorder()
+    def inorder(self):
+        """ left -> root -> right"""
+        if self.lchild:
+            self.lchild.inorder()
+        print(self.key)
+        if self.rchild:
+            self.rchild.inorder()
+    def postorder(self):
+      
+        if self.lchild :
+           self.lchild.postorder() 
+        if self.rchild:
+           self.rchild.postorder()
+        print(self.key)
+        
+
+root=BST(10)
+#root.lchild=BST(5)
+#print(root.key,root.lchild.lchild,root.rchild)
+list1=[6,3,1,6,98,3,7]
+for i in list1:
+    root.insert(i)
+
+#root.search(2)
+print("preorder traversal")
+root.preorder()
+print("inorder traversal")
+root.inorder()
+print("postorder traversal")
+root.postorder()
         
 
 root=BST(10)
